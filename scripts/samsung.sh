6edf356e0bd2503215ff9b7b45037ec6467e489c
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-01 19:37:59 +0700

    Update samsung.sh

--- scripts/samsung.sh
+++ scripts/samsung.sh
@@ -1,89 +1,98 @@
 #!/usr/bin/env bash
-
-# Dependencies
-rm -rf kernel
-git clone $REPO -b $BRANCH kernel
-cd kernel
-echo "Cloning GCC"
-git clone https://github.com/najahiiii/aarch64-linux-gnu.git -b 4.9-mirror --depth=1 gcc
-git clone https://github.com/najahiiii/aarch64-linux-gnu.git -b 4.9-32-mirror --depth=1 gcc32
+echo "Nuke previous toolchains"
+rm -rf toolchain out AnyKernel
+echo "cleaned up"
+echo "Cloning dependencies"
+git clone https://github.com/najahiiii/priv-toolchains -b elf/gcc-10.0.0/arm --depth=1 gcc32
+git clone https://github.com/najahiiii/priv-toolchains -b elf/gcc-10.0.0/arm64 --depth=1 gcc
+git clone https://github.com/najahiiii/AnyKernel3.git -b asus --depth=1 AnyKernel
 echo "Done"
+if [ "$is_test" = true ]; then
+     echo "Its alpha test build"
+     unset chat_id
+     unset token
+     export chat_id=${my_id}
+     export token=${nToken}
+else
+     echo "Its beta release build"
+fi
+GCC="$(pwd)/gcc/bin/aarch64-elf-"
+GCC32="$(pwd)/gcc32/bin/arm-eabi-"
+SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-GCC="$(pwd)/gcc/bin/aarch64-linux-android-"
-GCC32="$(pwd)/gcc/bin/arm-linux-androideabi-"
-tanggal=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
+TANGGAL=$(date +'%H%M-%d%m%y')
+JOBS=$(nproc)
+LOADS=$(nproc)
 START=$(date +"%s")
 export ARCH=arm64
 export KBUILD_BUILD_USER=malkist
 export KBUILD_BUILD_HOST=android
 # sticker plox
 function sticker() {
-        curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-                        -d sticker="CAADBQADJgEAAkMQsyKKVBNIRBu80wI" \
-                        -d chat_id=$chat_id
+    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+        -d sticker="CAADBQADKwEAAkMQsyJtEJHSjxmH-wI" \
+        -d chat_id=$chat_id
 }
 # Send info plox channel
 function sendinfo() {
-        curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-                        -d chat_id=$chat_id \
-                        -d "disable_web_page_preview=true" \
-                        -d "parse_mode=html" \
-                        -d text="<b>Teletubies</b> CI Triggered%0ABuild started on <code>Drone CI/CD</code>%0AFor device <b>Samsung J6primelte</b>%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code> (Android 10-11)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(TZ=Asia/Jakarta date)</code>%0A<b>Build Status:</b> #Stable"
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>ChipsKernel CAF EAS</b> CI Triggered%0ABuild started on <code>Drone CI/CD</code>%0AFor device <b>Asus Max Pro M1</b> (X00T/D)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code> (Android 9.0/Pie)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Nightly"
 }
 # Send private info
 function sendpriv() {
-        curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-                        -d chat_id=$priv_id \
-                        -d "disable_web_page_preview=true" \
-                        -d "parse_mode=html" \
-                        -d text="ChipsKernel CI Started%0ADrone triggered by: <code>${DRONE_BUILD_EVENT}</code> event%0AJob name: <code>Baking</code>%0ACommit point: <a href='${DRONE_COMMIT_LINK}'>$(git log --pretty=format:'"%h : %s"' -1)</a>%0A<b>Pipeline jobs</b> <a href='https://cloud.drone.io/najahiiii/moaikernal/${DRONE_BUILD_NUMBER}'>here</a>"
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$priv_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="ChipsKernel CI Started%0ADrone triggered by: <code>${DRONE_BUILD_EVENT}</code> event%0AJob name: Chips%0ACommit point: <a href='${DRONE_COMMIT_LINK}'>$(git log --pretty=format:'"%h : %s"' -1)</a>%0A<b>Pipeline jobs</b> <a href='https://cloud.drone.io/najahiiii/kernel_asus_sdm660/${DRONE_BUILD_NUMBER}'>here</a>"
 }
 # Push kernel to channel
 function push() {
-        cd AnyKernel
-	ZIP=$(echo *.zip)
-	curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-			-F chat_id="$chat_id" \
-			-F "disable_web_page_preview=true" \
-			-F "parse_mode=html" \
-			-F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Samsung J6primelte</b> | <a href='${HASIL}'>Logs</a>"
+    cd AnyKernel || exit 1
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>ASUS ZENFONE MAX PRO M1 (X00T/D)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 }
 # Function upload logs to my own server paste
 function paste() {
-        cat build.log | curl -F 'chips=<-' https://chipslogs.herokuapp.com > link
-        HASIL="$(cat link)"
+    cat build.log | curl -F 'chips=<-' https://chipslogs.herokuapp.com > link
+    HASIL="$(cat link)"
 }
 # Fin Error
 function finerr() {
-	paste
-        curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-			-d chat_id="$chat_id" \
-			-d "disable_web_page_preview=true" \
-			-d "parse_mode=markdown" \
-			-d text="Job Baking Chips throw an error(s) | **Build logs** [here](${HASIL})"
-        exit 1
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Job Baking Chips throw an error(s)"
+    exit 1
 }
 # Compile plox
 function compile() {
-        make -s -C $(pwd) O=out teletubies_defconfig
-        make -C $(pwd) CROSS_COMPILE=${GCC} CROSS_COMPILE=${GCC32} O=out -j32 -l32 2>&1| tee build.log
-    if ! [ -a "$IMAGE" ]; then
-        finderr
-        exit 1
-    fi
-
-    git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
+    make -s -C $(pwd) -j$JOBS O=out teletubies_defconfig
+    make -C $(pwd) CROSS_COMPILE="${GCC}" CROSS_COMPILE_ARM32="${GCC32}" O=out -j$JOBS -l$LOADS 2>&1| tee build.log
+        if ! [ -a "$IMAGE" ]; then
+            finerr
+            exit 1
+        fi
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
-zipping() {
+function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 kernel-testing-"${BRANCH}"-"${CODENAME}"-"${DATE}".zip ./*
-    cd ..
+    if [ "$is_test" = true ]; then
+        zip -r9 ChipsKernel-GCC-EAS-Alpha-"${TANGGAL}-${SHA}".zip * -x .git README.md *.zip
+    else
+        zip -r9 ChipsKernel-GCC-EAS-"${TANGGAL}-${SHA}".zip * -x .git README.md *.zip
+    fi #ngentod
+    cd .. #well
 }
-sticker
-sendpriv
-sendinfo
 compile
 zipping
 END=$(date +"%s")
