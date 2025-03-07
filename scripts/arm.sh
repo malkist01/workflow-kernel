b4e8ec7ac36a610a63e2ccd53ff35f3db1576ae1
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-07 10:15:48 +0700

    Add files via upload

--- scripts/arm.sh
+++ scripts/arm.sh
@@ -1,111 +1,134 @@
 #!/usr/bin/env bash
+
+# Dependencies
 rm -rf kernel
-git clone $REPO -b $BRANCH kernel 
+git clone $REPO -b $BRANCH kernel
 cd kernel
-echo "Cloning dependencies"
-git clone -q -j32 https://github.com/Andromax-Devices/RastaMod69-Clang.git --depth=1 -b clang-12.0 clang
-git clone -q -j32 https://github.com/najahiiii/priv-toolchains.git --depth=1 -b non-elf/gcc-10.0.0/arm gcc32
-git clone -q -j32 https://github.com/najahiiii/priv-toolchains.git --depth=1 -b non-elf/gcc-10.0.0/arm64 gcc
-git clone -q -j32 https://github.com/malkist01/AnyKernel3.git -b master --depth=1 AnyKernel
-echo "Done"
-if [ "$is_test" = true ]; then
-     echo "Its alpha test build"
-     unset chat_id
-     unset token
-     export chat_id=${my_id}
-     export token=${nToken}
-else
-     echo "Its beta release build"
-fi
-GCC="$(pwd)/gcc/bin/aarch64-linux-gnu-"
-GCC32="$(pwd)/gcc32/bin/arm-linux-gnueabi-"
-CT="$(pwd)/clang/bin/clang"
-SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-TANGGAL=$(date +'%H%M-%d%m%y')
+
+clang() {
+    rm -rf clang
+    echo "Cloning clang"
+    if [ ! -d "clang" ]; then
+        git clone https://github.com/malkist01/android_kernel_samsung_msm8917.git  -b main --depth=1 clang
+        KBUILD_COMPILER_STRING=""
+        PATH="${PWD}/clang/bin:${PATH}"
+    fi
+    sudo apt install -y ccache
+    echo "Done"
+}
+
+IMAGE=$(pwd)/out/arch/arm/boot/Image.gz-dtb
+DATE=$(date +"%Y%m%d-%H%M")
 START=$(date +"%s")
-export ARCH=arm64
-export KBUILD_BUILD_USER=Najahi
-export KBUILD_BUILD_HOST=NusantaraDevs
-export LD_LIBRARY_PATH="$(pwd)/clang/lib64:$PATH"
-# sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAADBQADKwEAAkMQsyJtEJHSjxmH-wI" \
-        -d chat_id=$chat_id
+KERNEL_DIR=$(pwd)
+CACHE=1
+export CACHE
+export KBUILD_COMPILER_STRING
+ARCH=arm
+export ARCH
+KBUILD_BUILD_HOST="android-server"
+export KBUILD_BUILD_HOST
+KBUILD_BUILD_USER="malkist"
+export KBUILD_BUILD_USER
+DEVICE="samsung"
+export DEVICE
+CODENAME="j6primelte"
+export CODENAME
+# DEFCONFIG=""
+#DEFCONFIG_COMMON="vendor/msm8953-romi_defconfig"
+DEFCONFIG_DEVICE="teletubies_defconfig"
+#export DEFCONFIG_COMMON
+export DEFCONFIG_DEVICE
+COMMIT_HASH=$(git rev-parse --short HEAD)
+export COMMIT_HASH
+PROCS=$(nproc --all)
+export PROCS
+STATUS=STABLE
+export STATUS
+source "${HOME}"/.bashrc && source "${HOME}"/.profile
+if [ $CACHE = 1 ]; then
+    ccache -M 100G
+    export USE_CCACHE=1
+fi
+LC_ALL=C
+export LC_ALL
+
+tg() {
+    curl -sX POST https://api.telegram.org/bot"${token}"/sendMessage -d chat_id="${chat_id}" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null
 }
-# Send info plox channel
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>ChipsKernel CAF EAS</b> CI Triggered%0ABuild started on <code>Drone CI/CD</code>%0AFor device <b>Asus Max Pro M1</b> (X00T/D)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code> (Android 9.0/Pie)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Nightly"
+
+tgs() {
+    MD5=$(md5sum "$1" | cut -d' ' -f1)
+    curl -fsSL -X POST -F document=@"$1" https://api.telegram.org/bot"${token}"/sendDocument \
+        -F "chat_id=${chat_id}" \
+        -F "parse_mode=Markdown" \
+        -F "caption=$2 | *MD5*: \`$MD5\`"
 }
-# Send private info
-function sendpriv() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$priv_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="ChipsKernel CI Started%0ADrone triggered by: <code>${DRONE_BUILD_EVENT}</code> event%0AJob name: Chips%0ACommit point: <a href='${DRONE_COMMIT_LINK}'>$(git log --pretty=format:'"%h : %s"' -1)</a>%0A<b>Pipeline jobs</b> <a href='https://cloud.drone.io/najahiiii/kernel_asus_sdm660/${DRONE_BUILD_NUMBER}'>here</a>"
+
+# Send Build Info
+sendinfo() {
+    tg "
+• sirCompiler Action •
+*Building on*: \`Github actions\`
+*Date*: \`${DATE}\`
+*Device*: \`${DEVICE} (${CODENAME})\`
+*Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
+*Last Commit*: [${COMMIT_HASH}](${REPO}/commit/${COMMIT_HASH})
+*Compiler*: \`${KBUILD_COMPILER_STRING}\`
+*Build Status*: \`${STATUS}\`"
 }
+
 # Push kernel to channel
-function push() {
+push() {
     cd AnyKernel || exit 1
     ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>ASUS ZENFONE MAX PRO M1 (X00T/D)</b> | <b>$(${CT} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+    tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For *${DEVICE} (${CODENAME})* | ${KBUILD_COMPILER_STRING}"
 }
-# Function upload logs to my own server paste
-function paste() {
-    cat build.log | curl -F 'chips=<-' https://chipslogs.herokuapp.com > link
-    HASIL="$(cat link)"
-}
-# Fin Error
-function finerr() {
+
+# Catch Error
+finderr() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
-        -d text="Job Baking Chips throw an error(s)"
+        -d text="Build throw an error(s)"
     exit 1
 }
-# Compile plox
-function compile() {
-    make -s -C $(pwd) -j48 O=out j6primelte_defconfig
-    PATH="$(pwd)/clang/bin:${PATH}" \
-    make -C $(pwd) CC=${CT} \
-            CROSS_COMPILE=${GCC} \
-            CROSS_COMPILE_ARM32=${GCC32} \
-            CLANG_TRIPLE=aarch64-linux-gnu- \
-            O=out \
-            -j60 \
-            -l50 2>&1| tee build.log
-            if ! [ -a "$IMAGE" ]; then
-                finerr
-                exit 1
-            fi
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+
+# Compile
+compile() {
+
+    if [ -d "out" ]; then
+        rm -rf out && mkdir -p out
+    fi
+
+    make O=out ARCH="${ARCH}"
+    make "$DEFCONFIG_COMMON" O=out
+    make "$DEFCONFIG_DEVICE" O=out
+    make -j"${PROCS}" O=out \
+        ARCH=$ARCH \
+        CROSS_COMPILE_COMPILE=arm-linux-androideabi- \
+     KCFLAGS=-mno-android \
+
+    if ! [ -a "$IMAGE" ]; then
+        finderr
+        exit 1
+    fi
+
+    git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
+    cp out/arch/arm/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
-function zipping() {
+zipping() {
     cd AnyKernel || exit 1
-    rm *.zip
-    if [ "$is_test" = true ]; then
-        zip -r9 ChipsKernel-NusantaraDevsTC-EAS-Alpha-"${TANGGAL}-${SHA}".zip * -x .git README.md *.zip
-    else
-        zip -r9 ChipsKernel-NusantaraDevsTC-EAS-"${TANGGAL}-${SHA}".zip * -x .git README.md *.zip
-    fi #ngentod
-    cd .. #well
+    zip -r9 Teletubies-"${CODENAME}"-"${DATE}".zip ./*
+    cd ..
 }
-sticker
+
+clang
 sendinfo
 compile
 zipping
 END=$(date +"%s")
-DIFF=$(($END - $START))
+DIFF=$((END - START))
 push
