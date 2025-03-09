cdb18517ee82dfe41f1880de58c54842a4ea81aa
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-09 17:36:16 +0700

    Update samsung.sh

--- scripts/samsung.sh
+++ scripts/samsung.sh
@@ -2,7 +2,6 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.3
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
