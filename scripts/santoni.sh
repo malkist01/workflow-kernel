390a5509674e59493b455322b095bfeabca1428f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-09 19:45:14 +0700

    Update santoni.sh

--- scripts/santoni.sh
+++ scripts/santoni.sh
@@ -9,7 +9,7 @@ clang() {
     rm -rf clang
     echo "Cloning clang"
     if [ ! -d "clang" ]; then
-        git clone https://gitlab.com/clangsantoni/zyc_clang.git  -b 21 --depth=1 clang
+        git clone https://github.com/malkist01/LiuNian-clang.git  -b master --depth=1 clang
         KBUILD_COMPILER_STRING=""
         PATH="${PWD}/clang/bin:${PATH}"
     fi
