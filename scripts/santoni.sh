8713b6ad3c50f14629c405b2df37a7ff33d2c03c
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-09 20:55:18 +0700

    Update santoni.sh

--- scripts/santoni.sh
+++ scripts/santoni.sh
@@ -9,7 +9,7 @@ clang() {
     rm -rf clang
     echo "Cloning clang"
     if [ ! -d "clang" ]; then
-        git clone git@gitlab.com:clangsantoni/zyc_clang.git  -b 10 --depth=1 clang
+        git clone https://github.com/malkist01/proton-clang.git -b proton-clang-11 --depth=1 clang
         KBUILD_COMPILER_STRING=""
         PATH="${PWD}/clang/bin:${PATH}"
     fi
