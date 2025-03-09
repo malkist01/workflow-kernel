0fee6d850868a4635f60fea3a1396d6aefb162bb
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-09 19:57:29 +0700

    Update santoni.sh

--- scripts/santoni.sh
+++ scripts/santoni.sh
@@ -108,13 +108,6 @@ compile() {
     make -j"${PROCS}" O=out \
         ARCH=$ARCH \
         CC=clang \
-        LD=ld.lld \
-	AR=llvm-ar \
-	AS=llvm-as \
-	NM=llvm-nm \
-	OBJCOPY=llvm-objcopy \
-	OBJDUMP=llvm-objdump \
-	STRIP=llvm-strip \
 	CROSS_COMPILE=aarch64-linux-android- \
 	CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
      	CLANG_TRIPLE=aarch64-linux-gnu- \
