f8a2fbfe22c743e964a7f493b3902114919db47a
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-14 18:34:06 +0700

    Update arm.sh

--- scripts/arm.sh
+++ scripts/arm.sh
@@ -1,26 +1,81 @@
-#!/bin/bash
+#!/usr/bin/env bash
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
-
-export ARCH="arm"
-export CROSS_COMPILE="$(pwd)/kernel/gcc/bin/arm-linux-gnu-"
+echo "Nuke previous toolchains"
+rm -rf toolchain out AnyKernel
+echo "cleaned up"
+echo "Cloning dependencies"
+echo "Done"
+if [ "$is_test" = true ]; then
+     echo "Its alpha test build"
+     unset chat_id
+     unset token
+     export chat_id=${my_id}
+     export token=${nToken}
+else
+     echo "Its beta release build"
+fi
+SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
+IMAGE=$(pwd)/out/arch/arm/boot/Image.gz-dtb
+TANGGAL=$(date +'%H%M-%d%m%y')
+START=$(date +"%s")
+export CROSS_COMPILE="$(pwd)/kernel/gcc/bin/aarch64-linux-gnu-"
 export PATH="$(pwd)/kernel/gcc/bin:$PATH"
-export KBUILD_BUILD_USER="malkist"
-export KBUILD_BUILD_HOST="android"
-export USE_CCACHE=1
-export CACHE_DIR=~/.ccache
-curl -F chat_id="-1002287610863" -F text="Compiling New Commits..." https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
-tanggal=$(date +'%m%d-%H%M')
-rm -rf output
-mkdir output
-make -C $(pwd) O=output teletubies_defconfig
-make -j8 -C $(pwd) O=output
-if [ ! -f output/arch/arm/boot/Image.gz-dtb ]; then
-    echo "Teletubies, Compiling Failed"
-    curl -F chat_id="-1002287610863" -F text="Teletubies, Compile Fail :(" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
+export ARCH=arm
+export KBUILD_BUILD_USER=malkist
+export KBUILD_BUILD_HOST=android
+# sticker plox
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+        -d sticker="CAADBQADKwEAAkMQsyJtEJHSjxmH-wI" \
+        -d chat_id=$chat_id
+}
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>ChipsKernel CAF EAS</b> CI Triggered%0ABuild started on <code>Drone CI/CD</code>%0AFor device <b>Samsung</b> (J6PRIMELTE)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code> (Android 10-11)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Nightly"
+}
+# Send private info
+function sendpriv() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$priv_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="ChipsKernel CI Started%0ADrone triggered by: <code>${DRONE_BUILD_EVENT}</code> event%0AJob name: Chips%0ACommit point: <a href='${DRONE_COMMIT_LINK}'>$(git log --pretty=format:'"%h : %s"' -1)</a>%0A<b>Pipeline jobs</b> <a href='https://cloud.drone.io/najahiiii/kernel_asus_sdm660/${DRONE_BUILD_NUMBER}'>here</a>"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel || exit 1
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Samsung J6+</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+}
+# Function upload logs to my own server paste
+function paste() {
+    cat build.log | curl -F 'chips=<-' https://chipslogs.herokuapp.com > link
+    HASIL="$(cat link)"
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Job Baking Chips throw an error(s)"
+    exit 1
+}
+# Compile plox
+function compile() {
+     make -C $(pwd) O=out teletubies_defconfig
+     make -j8 -C $(pwd) O=out
 
-else 
      if ! [ -a "$IMAGE" ]; then
         finderr
         exit 1
@@ -28,18 +83,15 @@ else
 
     git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
     cp out/arch/arm/boot/Image.gz-dtb AnyKernel
-
+}
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Teletubies-arm"${CODENAME}"-"${DATE}".zip ./*
+    zip -r9 Teletubies"${CODENAME}"-"${DATE}".zip ./*
     cd ..
-
-echo "Yeehaa Booooi, Compiling Success!"
-curl -F chat_id="-1002287610863" -F document=@"Teletubies-${tanggal}.zip" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendDocument
-curl -F chat_id="-1002287610863" -F text="Teletubies, Compile Success :)" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
-
-curl -F chat_id="-1002287610863" -F text="Whats New ?
-$(git log --oneline --decorate --color --pretty=%s --first-parent -3)" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
-
-curl -F chat_id="-1002287610863" -F sticker="CAADBQADZwADqZrmFoa87YicX2hwAg" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendSticker
+}
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
