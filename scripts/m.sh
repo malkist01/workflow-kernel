89642eff8fbb22df9b611cdde6f5303a6997f48e
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-14 11:46:06 +0700

    Update m.sh

--- scripts/m.sh
+++ scripts/m.sh
@@ -1,99 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/bash
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
-echo "Nuke previous toolchains"
-rm -rf toolchain out AnyKernel
-echo "cleaned up"
-echo "Cloning dependencies"
-git clone --depth=1 -b master https://github.com/malkist01/gcc-11.x-aarch64-linux-gnu.git gcc
-echo "Done"
-if [ "$is_test" = true ]; then
-     echo "Its alpha test build"
-     unset chat_id
-     unset token
-     export chat_id=${my_id}
-     export token=${nToken}
-else
-     echo "Its beta release build"
-fi
-GCC="$(pwd)/gcc/bin/aarch64-linux-gnu-"
-SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-TANGGAL=$(date +'%H%M-%d%m%y')
-JOBS=$(nproc)
-LOADS=$(nproc)
-START=$(date +"%s")
-DEF=teletubies_defconfig
+wget https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
+tar xvf gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
+make clean distclean mrproper
 export ARCH=arm64
-export KBUILD_BUILD_USER=malkist
-export KBUILD_BUILD_HOST=android
-# sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAADBQADKwEAAkMQsyJtEJHSjxmH-wI" \
-        -d chat_id=$chat_id
-}
-# Send info plox channel
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>ChipsKernel CAF EAS</b> CI Triggered%0ABuild started on <code>Drone CI/CD</code>%0AFor device <b>Samsung</b> (J6PRIMELTE)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code> (Android 10-11)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Nightly"
-}
-# Send private info
-function sendpriv() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$priv_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="ChipsKernel CI Started%0ADrone triggered by: <code>${DRONE_BUILD_EVENT}</code> event%0AJob name: Chips%0ACommit point: <a href='${DRONE_COMMIT_LINK}'>$(git log --pretty=format:'"%h : %s"' -1)</a>%0A<b>Pipeline jobs</b> <a href='https://cloud.drone.io/najahiiii/kernel_asus_sdm660/${DRONE_BUILD_NUMBER}'>here</a>"
-}
-# Push kernel to channel
-function push() {
-    cd AnyKernel || exit 1
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>ASUS ZENFONE MAX PRO M1 (X00T/D)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
-}
-# Function upload logs to my own server paste
-function paste() {
-    cat build.log | curl -F 'chips=<-' https://chipslogs.herokuapp.com > link
-    HASIL="$(cat link)"
-}
-# Fin Error
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Job Baking Chips throw an error(s)"
-    exit 1
-}
-# Compile plox
-function compile() {
-    make -s -C $(pwd) -j$JOBS O=out "${DEF}"
-    make -C $(pwd) CROSS_COMPILE="${GCC}" O=out -j$JOBS -l$LOADS 2>&1| tee build.log
-     if ! [ -a "$IMAGE" ]; then
-        finderr
-        exit 1
-    fi
+export CROSS_COMPILE=/home/runner/CrappyKernel/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export KBUILD_BUILD_USER=ProtoChuz
+export KBUILD_BUILD_HOST=SemaphoreCI
+export USE_CCACHE=1
+export CACHE_DIR=~/.ccache
+curl -F chat_id="-1002287610863" -F text="Compiling New Commits..." https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
+tanggal=$(date +'%m%d-%H%M')
+rm -rf output
+mkdir output
+make -C $(pwd) O=output teletubies_defconfig
+make -j8 -C $(pwd) O=output
+if [ ! -f output/arch/arm64/boot/Image.gz-dtb ]; then
+    echo "HolyCrap, Compiling Failed"
+    curl -F chat_id="-1002287610863" -F text="HolyCrap, Compile Fail :(" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
 
-    git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-}
-# Zipping
-zipping() {
-    cd AnyKernel || exit 1
-    zip -r9 Teletubies"${CODENAME}"-"${DATE}".zip ./*
-    cd ..
-}
-compile
-zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
-push
+else 
+cp output/arch/arm64/boot/Image.gz-dtb AnyKernel2/zImage
+cd AnyKernel2
+rm -rf *.zip
+zip -r9 CrappyKernel-Liquor-${tanggal}.zip * -x README.md CrappyKernel-Liquor--${tanggal}.zip
+echo "Yeehaa Booooi, Compiling Success!"
+curl -F chat_id="-1002287610863" -F document=@"CrappyKernel-Liquor-${tanggal}.zip" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendDocument
+curl -F chat_id="-1002287610863" -F text="HolyCrap, Compile Success :)" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
+curl -F chat_id="-1002287610863" -F text="Whats New ?
+$(git log --oneline --decorate --color --pretty=%s --first-parent -3)" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendMessage
+
+fi
+curl -F chat_id="-1002287610863" -F sticker="CAADBQADZwADqZrmFoa87YicX2hwAg" https://api.telegram.org/bot7596553794:AAGoeg4VypmUfBqfUML5VWt5mjivN5-3ah8/sendSticker
